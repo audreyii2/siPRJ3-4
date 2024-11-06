5a70d9566a71db56ba85113dbe4e11030a8ab07a
audreyii2 <aulia5lli3nn@gmail.com>
2024-11-06 14:50:48 -0500

    Add files via upload

--- js/fGallery.js
+++ js/fGallery.js
@@ -5,14 +5,14 @@ let currentImageIndex = 0;
 function preloadImages() {
     images = [
         // Add more gallery images with their specific styles here
-        { src: "IMG_9347.jpg", alt: "Gallery Display of Event, runner #1012" },
-        { src: "IMG_9345_V2F_2024-09-14_12-39-43_867.jpg", alt: "Gallery Display of Event, intense competition" },
-        { src: "IMG_4184(1).jpg", alt: "Gallery Display of Event, medallists" },
-        { src: "IMG_4181.jpg", alt: "Gallery Display of Event, team members preparing their nerves for a big competition" },
-        { src: "IMG_4178.jpg", alt: "Gallery Display of Event, runners exhausted by the sprint" },
-        { src: "IMG_4177.jpg", alt: "Gallery Display of Event, runners keeping up good pace" },
-        { src: "IMG_4176.jpg", alt: "Gallery Display of Event, marching, happy candidates" },
-        { src: "IMG_4175.jpg", alt: "Gallery Display of Event, candidates chilling out before the competition" }
+        { src: "img/IMG_9347.jpg", alt: "Gallery Display of Event, runner #1012" },
+        { src: "img/IMG_9345_V2F_2024-09-14_12-39-43_867.jpg", alt: "Gallery Display of Event, intense competition" },
+        { src: "img/IMG_4184(1).jpg", alt: "Gallery Display of Event, medallists" },
+        { src: "img/IMG_4181.jpg", alt: "Gallery Display of Event, team members preparing their nerves for a big competition" },
+        { src: "img/IMG_4178.jpg", alt: "Gallery Display of Event, runners exhausted by the sprint" },
+        { src: "img/IMG_4177.jpg", alt: "Gallery Display of Event, runners keeping up good pace" },
+        { src: "img/IMG_4176.jpg", alt: "Gallery Display of Event, marching, happy candidates" },
+        { src: "img/IMG_4175.jpg", alt: "Gallery Display of Event, candidates chilling out before the competition" }
     ];
 
     images.sort((a, b) => {
