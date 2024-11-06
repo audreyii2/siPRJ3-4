39d2dad7a25fcb6c89d2c72895840c2c93f26cda
audreyii2 <aulia5lli3nn@gmail.com>
2024-11-06 13:03:44 -0500

    Add files via upload

---
+++ js/imgFinder.js
@@ -0,0 +1,8 @@
+// Select all images on the page
+document.querySelectorAll('img').forEach(img => {
+    img.onerror = function() {
+      this.onerror = null; // Prevents infinite loop if default image missing
+      this.src = 'path/to/default.jpg';
+      this.alt = ""
+    };
+  });
